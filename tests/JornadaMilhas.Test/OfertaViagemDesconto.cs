d93714003b883d960e395a74859b7e5f67921f5b
CassioJhones <cassio.bjhones@gmail.com>
2024-07-25 03:01:02 -0300

    TDD - Teste primeiro, implementa depois.

---
+++ tests/JornadaMilhas.Test/OfertaViagemDesconto.cs
@@ -0,0 +1,30 @@
+﻿using JornadaMilhasV1.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JornadaMilhas.Test;
+public class OfertaViagemDesconto
+{
+    [Fact]
+    public void RetornaPrecoAtualizadoQuandoTemDesconto()
+    {
+        //arrange
+        Rota rota = new Rota("OrigemA", "DestinoB");
+        Periodo periodo = new Periodo(new DateTime(2024, 07, 25), new DateTime(2024, 07,30));
+        double precoOriginal = 100.0;
+        double desconto = 20.00;
+        double precoComDesconto = precoOriginal- desconto;
+
+        OfertaViagem oferta = new OfertaViagem(rota, periodo, precoOriginal);
+        
+
+        //act
+        oferta.Desconto = desconto;
+
+        //assert
+        Assert.Equal(precoComDesconto, oferta.Preco);
+    }
+}
