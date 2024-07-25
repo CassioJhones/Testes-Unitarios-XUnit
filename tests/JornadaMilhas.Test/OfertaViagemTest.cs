735d51db88927e5a19a0c64aa77e3599a01c30a1
CassioJhones <cassio.bjhones@gmail.com>
2024-07-25 01:53:43 -0300

    Rota Nula

--- tests/JornadaMilhas.Test/OfertaViagemTest.cs
+++ tests/JornadaMilhas.Test/OfertaViagemTest.cs
@@ -7,11 +7,25 @@ public class OfertaViagemTest
     [Fact]
     public void TestandoOfertaValida()
     {
-
         Rota rota = new Rota("OrigemTeste","DestinoTeste");
         Periodo periodo = new Periodo(new DateTime(2024, 2, 1), new DateTime(2024, 2, 5));
         double preco = 100.0;
-        var validacao = true;
+        bool validacao = true;
 
+        OfertaViagem oferta = new OfertaViagem(rota, periodo,preco);
+        Assert.Equal(validacao, oferta.EhValido);
+
+    }
+
+    [Fact]
+    public void TestandoOfertaRotaNula()
+    {
+        Rota rota = null;
+        Periodo periodo = new Periodo(new DateTime(2024, 2, 1), new DateTime(2024, 2, 5));
+        double preco = 100.0;
+
+        OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
+        Assert.Contains("A oferta de viagem não possui rota ou período válidos.", oferta.Erros.Sumario);
+        Assert.False(oferta.EhValido);
     }
 }
