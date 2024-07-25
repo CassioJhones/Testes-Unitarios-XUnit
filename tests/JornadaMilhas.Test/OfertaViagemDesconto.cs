2d81c9454adbc4ff5ad4d1ff58944989aaa226b1
CassioJhones <cassio.bjhones@gmail.com>
2024-07-25 03:15:57 -0300

    Teste para desconto Maior

--- tests/JornadaMilhas.Test/OfertaViagemDesconto.cs
+++ tests/JornadaMilhas.Test/OfertaViagemDesconto.cs
@@ -13,18 +13,36 @@ public class OfertaViagemDesconto
     {
         //arrange
         Rota rota = new Rota("OrigemA", "DestinoB");
-        Periodo periodo = new Periodo(new DateTime(2024, 07, 25), new DateTime(2024, 07,30));
+        Periodo periodo = new Periodo(new DateTime(2024, 07, 25), new DateTime(2024, 07, 30));
         double precoOriginal = 100.0;
         double desconto = 20.00;
-        double precoComDesconto = precoOriginal- desconto;
+        double precoComDesconto = precoOriginal - desconto;
 
         OfertaViagem oferta = new OfertaViagem(rota, periodo, precoOriginal);
-        
 
         //act
         oferta.Desconto = desconto;
 
         //assert
         Assert.Equal(precoComDesconto, oferta.Preco);
+    }
+
+    [Fact]
+    public void RetornaDescontoMaximoQuandoValorMaiorQuePreco()
+    {
+        //arrange
+        Rota rota = new Rota("OrigemA", "DestinoB");
+        Periodo periodo = new Periodo(new DateTime(2024, 07, 25), new DateTime(2024, 07, 30));
+        double precoOriginal = 100.00;
+        double desconto = 120.00;
+        double precoComDesconto = 30.00;
+
+        OfertaViagem oferta = new OfertaViagem(rota, periodo, precoOriginal);
+
+        //act
+        oferta.Desconto = desconto;
+
+        //assert
+        Assert.Equal(precoComDesconto, oferta.Preco, 0.001);
     }
 }
