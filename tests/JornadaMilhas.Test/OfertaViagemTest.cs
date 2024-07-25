631844ea44b568b7bd7527f6b55aa3c3502465bd
CassioJhones <cassio.bjhones@gmail.com>
2024-07-25 02:20:27 -0300

    teste para preço negativo

--- tests/JornadaMilhas.Test/OfertaViagemTest.cs
+++ tests/JornadaMilhas.Test/OfertaViagemTest.cs
@@ -6,15 +6,18 @@ public class OfertaViagemTest
 {
     [Fact]
     public void TestandoOfertaValida()
-    {
-        Rota rota = new Rota("OrigemTeste","DestinoTeste");
+    {//---PADRÃO AAA
+        //--CENARIO = ARRANGE
+        Rota rota = new Rota("OrigemTeste", "DestinoTeste");
         Periodo periodo = new Periodo(new DateTime(2024, 2, 1), new DateTime(2024, 2, 5));
         double preco = 100.0;
         bool validacao = true;
 
-        OfertaViagem oferta = new OfertaViagem(rota, periodo,preco);
-        Assert.Equal(validacao, oferta.EhValido);
+        //--AÇÃO - ACTION
+        OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
 
+        //--VALIDAÇÃO - ASSERT
+        Assert.Equal(validacao, oferta.EhValido);
     }
 
     [Fact]
@@ -27,5 +30,20 @@ public class OfertaViagemTest
         OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
         Assert.Contains("A oferta de viagem não possui rota ou período válidos.", oferta.Erros.Sumario);
         Assert.False(oferta.EhValido);
+    }
+
+    [Fact]
+    public void TestandoPrecoNegativo()
+    {
+        //--CENARIO = ARRANGE
+        Rota rota = new Rota("Rio Claro", "Araras");
+        Periodo periodo = new Periodo(new DateTime(2024, 2, 1), new DateTime(2024, 2, 5));
+        double preco = -320;
+
+        //--AÇÃO - ACTION
+        OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
+
+        //--VALIDAÇÃO - ASSERT
+        Assert.Contains("O preço da oferta de viagem deve ser maior que zero.", oferta.Erros.Sumario);
     }
 }
