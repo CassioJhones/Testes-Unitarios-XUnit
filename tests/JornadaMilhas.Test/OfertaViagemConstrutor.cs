a63abc356c261e0916a28c096a1b9086c4657b88
CassioJhones <cassio.bjhones@gmail.com>
2024-07-25 02:45:34 -0300

    notation Theory

--- tests/JornadaMilhas.Test/OfertaViagemConstrutor.cs
+++ tests/JornadaMilhas.Test/OfertaViagemConstrutor.cs
@@ -46,4 +46,24 @@ public class OfertaViagemConstrutor
         //--VALIDAÇÃO - ASSERT
         Assert.Contains("O preço da oferta de viagem deve ser maior que zero.", oferta.Erros.Sumario);
     }
+
+    //Permite varios possiveis Cenarios de teste
+    [Theory]
+    [InlineData("", null, "2024-01-01", "2024-02-02", 0, false)]
+    [InlineData("Rondonia", "Santos", "2024-02-01", "2024-02-07", 100, true)]
+    [InlineData(null, "Limeira", "2024-02-01", "2024-01-01", 100, false)]
+    [InlineData("Vitoria", "Sergipe", "2024-02-01", "2024-02-07", -5, false)]
+    [InlineData("Bahia", "Sao Paulo", "2024-02-01", "2024-02-07", 100, true)]
+    public void RetornaValidoseInvalidosDeAcordoComEntradas(string origem, string destino, string dataIda, string dataVolta, double preco, bool validacao)
+    {
+        //--CENARIO
+        Rota rota = new Rota(origem,destino);
+        Periodo periodo = new Periodo(DateTime.Parse(dataIda), DateTime.Parse(dataVolta));
+        
+        //--AÇÃO
+        OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
+
+        //--VALIDAÇÃO
+        Assert.Equal(validacao, oferta.EhValido);
+    }
 }
